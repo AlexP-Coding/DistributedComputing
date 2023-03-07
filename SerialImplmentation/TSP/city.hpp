3b97d2bb1cc11f8b187ee56436394efed2332f26
AlexP-Coding <alexandra.pato@tecnico.ulisboa.pt>
2023-03-07 21:25:53 +0000

    Changed neighbors and cost lists to vectors on City class. Changed file formatting.  Changed toString formatting.

--- SerialImplmentation/TSP/city.hpp
+++ SerialImplmentation/TSP/city.hpp
@@ -1,138 +1,131 @@
 #include <iostream>
-class City
-{
+#include <vector>
+
+class City {
     private:
-    int fillPos;
-    int id;
-    int numNeighbours;
-    int* neighbours;
-    double* costs;
-    double* highestCostEdges;
+        int nrNeighbours;
+        int id;
+        int maxNeighbours;
+        std::vector<int> neighbours;
+        std::vector<double> costs;
+        double* highestCostEdges;
 
-public:
-City(){}
+    public:
+        City(){}
 
-// Constructor of class City
-City(int id, int numCities)
-{
-    this->id = id;
-    this->numNeighbours = numCities-1;
-    this->neighbours = new int[numCities-1];
-    this->costs = new double[numCities-1];
-    this->highestCostEdges = new double[2];
-
-    fillPos=0;
-}
-
-// Deconstructor of class City
-~City()
-{
-    delete[] neighbours;
-    delete[] costs;
-}
-
-// Adds a neighbour to the 'neighbours' array and a cost at the same position on to the 'costs' array
-void addNeighbour(int neighbourId, double edgeCost)
-{
-    this->neighbours[fillPos]=neighbourId;
-    this->costs[fillPos]=edgeCost;
-    fillPos++;
-    updateHighestCostEdges(edgeCost);
-}
-
-// Returns the road cost between this city and the city specified by the 'neighbourId' argument
-double getEdgeCost(int neighbourId)
-{
-    for ( int i = 0 ; i < this->numNeighbours ; i++ )
-    {
-        if ( this->neighbours[i] == neighbourId )
+        // Constructor of class City
+        City(int city_id, int numCities)
         {
-            return this->costs[i];
+            id = city_id;
+            maxNeighbours = numCities-1;
+            highestCostEdges = new double[2];
+            nrNeighbours=0;
         }
-    }
-    return -1.0;
-}
 
-// Checks to see if the received edge id bigger than the ones already stored. If so it changes the smallest
-void updateHighestCostEdges(double newEdge)
-{
-    if( newEdge > this->highestCostEdges[0] )
-    {
-        this->highestCostEdges[1] = this->highestCostEdges[0];
-        this->highestCostEdges[0] = newEdge;
-    }
-    else
-    {
-        if ( newEdge > this->highestCostEdges[1] )
+        // Deconstructor of class City
+        ~City()
         {
-            this->highestCostEdges[1] = newEdge;
+            neighbours.clear();
+            costs.clear();
         }
-    }
-}
 
-double getHighestCostEdgesSum()
-{
-    return ( this->highestCostEdges[0] + this->highestCostEdges[1] ) ;
-}
+        // Adds a neighbour to the 'neighbours' array and a cost at the same position on to the 'costs' array
+        void addNeighbour(int neighbourId, double edgeCost)
+        {
+            neighbours.push_back(neighbourId);
+            costs.push_back(edgeCost);
+            printf("Neighbor added: id %d cost %lf\n", neighbours[nrNeighbours], costs[nrNeighbours]);
+            nrNeighbours++;
+            updateHighestCostEdges(edgeCost);
+        }
 
+        // Returns the road cost between this city and the city specified by the 'neighbourId' argument
+        double getEdgeCost(int neighbourId)
+        {
+            for ( int i = 0 ; i < nrNeighbours ; i++ )
+            {
+                if ( neighbours[i] == neighbourId )
+                {
+                    return costs[i];
+                }
+            }
+            return -1.0;
+        }
 
-    // Setter and getter functions for id
-    void setId(int id) {
-        this->id = id;
-    }
-    int getId() const {
-        return id;
-    }
-
-    // Setter and getter functions for numNeighbours
-    void setNumNeighbours(int numNeighbours) {
-        this->numNeighbours = numNeighbours;
-    }
-    int getNumNeighbours() const {
-        return numNeighbours;
-    }
-
-    // Setter and getter functions for neighbours
-    void setNeighbours(int* neighbours) {
-        this->neighbours = neighbours;
-    }
-    int* getNeighbours() const {
-        return neighbours;
-    }
-
-    // Setter and getter functions for costs
-    void setCosts(double* costs) {
-        this->costs = costs;
-    }
-    double* getCosts() const {
-        return costs;
-    }
-
-    // Setter and getter functions for highestCostEdges
-    void setHighestCostEdges(double* highestCostEdges) {
-        this->highestCostEdges = highestCostEdges;
-    }
-    double* getHighestCostEdges() const {
-        return highestCostEdges;
-    }
-
-    // toString method
-    void toString() {
-        std::cout << "Node(id=" << this->id << ", numNeighbours=" << this->numNeighbours << ", neighbours=[";
-        for (int i = 0; i < this->numNeighbours; i++) {
-            std::cout << this->neighbours[i];
-            if (i < this->numNeighbours - 1) {
-                std::cout << ", ";
+        // Checks to see if the received edge id bigger than the ones already stored. If so it changes the smallest
+        void updateHighestCostEdges(double newEdge)
+        {
+            if( newEdge > highestCostEdges[0] )
+            {
+                highestCostEdges[1] = highestCostEdges[0];
+                highestCostEdges[0] = newEdge;
+            }
+            else
+            {
+                if ( newEdge > highestCostEdges[1] )
+                {
+                    highestCostEdges[1] = newEdge;
+                }
             }
         }
-        std::cout << "], costs=[";
-        for (int i = 0; i < this->numNeighbours; i++) {
-            std::cout << this->costs[i];
-            if (i < this->numNeighbours - 1) {
-                std::cout << ", ";
-            }
+
+        double getHighestCostEdgesSum()
+        {
+            return (highestCostEdges[0] + highestCostEdges[1] ) ;
         }
-        std::cout << "], highestCostEdges=[" << this->highestCostEdges[0] << ", " << this->highestCostEdges[1] << "])";
-        return;
-    }
+
+
+        // Setter and getter functions for id
+        void setId(int city_id) {
+            id = city_id;
+        }
+        int getId() const {
+            return id;
+        }
+    
+        // Setter and getter functions for maxNeighbours
+        void setNrNeighbours(int city_nrNeighbours) {
+            nrNeighbours = city_nrNeighbours;
+        }
+        int getNrNeighbours() const {
+            return nrNeighbours;
+        }
+    
+        // Setter and getter functions for neighbours
+        void setNeighbours(std::vector<int> city_neighbours) {
+            neighbours = city_neighbours;
+        }
+        std::vector<int> getNeighbours() const {
+            return neighbours;
+        }
+    
+        // Setter and getter functions for costs
+        void setCosts(std::vector<double> city_costs) {
+            costs = city_costs;
+        }
+        std::vector<double> getCosts() const {
+            return costs;
+        }
+    
+        // Setter and getter functions for highestCostEdges
+        void setHighestCostEdges(double* city_highestCostEdges) {
+            highestCostEdges = city_highestCostEdges;
+        }
+        double* getHighestCostEdges() const {
+            return highestCostEdges;
+        }
+    
+        // toString method
+        void toString() {
+            printf("Node (id = %d, nrNeighbours = %d, neighbours = [", id, nrNeighbours);
+            for (int i = 0; i < nrNeighbours; i++) {
+                printf("%d, ", neighbours[i]);
+            }
+            printf("], costs=[");
+            for (int i = 0; i < maxNeighbours; i++) {
+                printf("%lf, ", costs[i]);
+            }
+            printf("], highestCostEdges=[%lf, %lf])\n", highestCostEdges[0], highestCostEdges[1]);
+            return;
+        }
 };
